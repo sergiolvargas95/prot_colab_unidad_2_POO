d70cc48d4270d156afbffe73c1112bf2be9d6e28
sr.davidsantiago@hotmail.com <sr.davidsantiago@hotmail.com>
2024-10-31 00:34:13 -0500

    Actualización de Clase

--- src/herenciaSimple/ejemploSeis/ejemploCinco/MainEjemploCinco.java
+++ src/herenciaSimple/ejemploSeis/ejemploCinco/MainEjemploCinco.java
@@ -5,14 +5,8 @@ public class MainEjemploCinco
     public static void main(String[] args) 
     {
         Animal animal = new Animal("Mamifero");
-        animal.mostrarEspecie();
-        animal.emitirSonido();
-        
-        System.out.println();
-
+    
         Perro perro = new Perro("Mamifero", "Labrador");
-        perro.mostrarEspecie();
-        perro.emitirSonido();
-        perro.ladrar();
+ 
     }
 }
