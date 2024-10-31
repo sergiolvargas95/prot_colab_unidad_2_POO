01d9779e7fc1e5f10e22cbaf8565d5c210e73119
sergiolvargas95 <sergiolvargas95@gmail.com>
2024-10-30 21:12:04 -0500

    cambios ejercicios sobre escritura

---
+++ src/herenciaSimple/ejercicioCuatro/ErrorMultiplesClase.java
@@ -0,0 +1,26 @@
+public class ErrorMultiplesClases
+{
+    class ClaseA
+    {
+        public void metodoA()
+        {
+            System.out.println("Metodo en ClaseA");
+        }
+    }
+
+    class ClaseB {
+        public void metodoB()
+        {
+            System.out.println("Metodo en ClaseB");
+        }
+    }
+
+
+    class ClaseC extends ClaseA, ClaseB
+    {
+        public void metodoC()
+        {
+            System.out.println("Metodo en ClaseC");
+        }
+    }
+}
